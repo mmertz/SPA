03a369e58742867efb98311b7ba02e4ba51b010c
Mitch <m.mertz93@gmail.com>
2018-12-01 11:56:14 +0100

    JWT init

--- routes/api.php
+++ routes/api.php
@@ -13,10 +13,6 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::apiResource('/question', 'QuestionController');
 
 Route::apiResource('/category', 'CategoryController');
@@ -25,3 +21,17 @@ Route::apiResource('/question/{question}/reply', 'ReplyController');
 
 Route::post('/like/{reply}', 'LikeController@LikeIt');
 Route::delete('/like/{reply}', 'LikeController@UnLikeIt');
+
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+});
